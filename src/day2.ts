b49fb809712b31a5615179e6971a153ba87d8305
David Schoen <dave@lyte.id.au>
2021-12-02 17:14:28 +1100

    day 2 part 2

--- src/day2.ts
+++ src/day2.ts
@@ -1,36 +1,105 @@
 export class Position {
   depth: number;
-  forward: number;
+  horizontal: number;
 
-  constructor(depth: number, forward: number) {
+  constructor(depth: number, horizontal: number) {
     this.depth = depth;
-    this.forward = forward;
+    this.horizontal = horizontal;
   }
 
   add(other: Position): Position {
-    return new Position(this.depth + other.depth, this.forward + other.forward);
+    return new Position(this.depth + other.depth, this.horizontal + other.horizontal);
+  }
+
+  move(move: Move): Position {
+    return new Position(this.depth + move.depth(), this.horizontal + move.horizontal());
   }
 }
 
-export function moveToPosition(move: string): Position {
+export class PositionWithAim extends Position {
+  aim: number;
+
+  constructor(depth: number, horizontal: number, aim: number) {
+    super(depth, horizontal);
+    this.aim = aim;
+  }
+
+  move(move: Move): PositionWithAim {
+    return new PositionWithAim(
+      this.depth + move.horizontal() * this.aim,
+      this.horizontal + move.horizontal(),
+      this.aim + move.aim()
+    );
+  }
+}
+
+export enum Direction {
+  Up, Down, Forward
+};
+
+export class Move {
+  direction: Direction;
+  distance: number;
+
+  constructor(direction: Direction, distance: number) {
+    this.direction = direction;
+    this.distance = distance;
+  }
+
+  depth() {
+    switch (this.direction) {
+      case Direction.Forward:
+        return 0;
+      case Direction.Up:
+        return 0 - this.distance;
+    }
+    return this.distance;
+  }
+
+  horizontal() {
+    switch (this.direction) {
+      case Direction.Forward:
+        return this.distance;
+    }
+    return 0;
+  }
+
+  aim() {
+    return this.depth();
+  }
+};
+
+const directionMap: { [key: string]: Direction } = {
+  'forward': Direction.Forward,
+  'up': Direction.Up,
+  'down': Direction.Down,
+}
+
+export function createMove(move: string): Move {
   let match = move.match(/^(forward|down|up) ([0-9]+)$/);
   if (!match) {
     throw new Error(`Could not parse move: ${move}`);
   }
-  let direction = match[1];
-  let distance = parseInt(match[2]);
-  let depth: number = direction == 'forward' ? 0 : ( direction == 'up' ? -1 : 1) * distance;
-  let forward: number = (direction == 'forward') ? distance : 0;
-  return new Position(depth, forward);
+  return new Move(directionMap[match[1]], parseInt(match[2]));
 }
 
 export function calculatePosition(movements: Array<string>): Position {
   let start = new Position(0, 0);
-  let movePositions = movements.map((move: string): Position => {
-    return moveToPosition(move);
+  let moves = movements.map((move: string): Move => {
+    return createMove(move);
   });
-  let final: Position = movePositions.reduce((previous: Position, current: Position): Position => {
-    return previous.add(current);
+  let final: Position = moves.reduce((previous: Position, current: Move): Position => {
+    return previous.move(current);
+  }, start);
+  return final;
+};
+export function calculatePositionWithAim(movements: Array<string>): PositionWithAim {
+  let start = new PositionWithAim(0, 0, 0);
+  let moves = movements.map((move: string): Move => {
+    return createMove(move);
+  });
+  let final: PositionWithAim = moves.reduce((previous: PositionWithAim, current: Move): PositionWithAim => {
+    return previous.move(current);
   }, start);
   return final;
 };
--- test/day2.spec.ts
+++ test/day2.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { calculatePosition, moveToPosition, Position } from '../src/day2';
+import { calculatePosition, calculatePositionWithAim, createMove, Direction, Move, Position, PositionWithAim } from '../src/day2';
 import * as fs from 'fs';
 
 describe('day 2', () => {
@@ -16,10 +16,10 @@ describe('day 2', () => {
   });
 
   describe("part 1", () => {
-    it("converts move to position", () => {
-      expect(moveToPosition('forward 5')).to.eql(new Position(0, 5));
-      expect(moveToPosition('down 3')).to.eql(new Position(3, 0));
-      expect(moveToPosition('up 2')).to.eql(new Position(-2, 0));
+    it("converts move", () => {
+      expect(createMove('forward 5')).to.eql(new Move(Direction.Forward, 5));
+      expect(createMove('down 3')).to.eql(new Move(Direction.Down, 3));
+      expect(createMove('up 2')).to.eql(new Move(Direction.Up, 2));
     })
     it("can add positions", () => {
       expect((new Position(12, 5)).add(new Position(-3, 6))).to.eql(new Position(9, 11));
@@ -31,6 +31,23 @@ describe('day 2', () => {
     it(`is ${answer}`, () => {
       expect(calculatePosition(realMoves)).to.eql(new Position(866, 1980));
       expect(866 * 1980).to.eql(answer);
+    })
+  });
+
+  describe("part 2", () => {
+    it("can move", () => {
+      expect((new PositionWithAim(12, 5, 0)).move(new Move(Direction.Up, 3))).to.eql(new PositionWithAim(12, 5, -3));
+      expect((new PositionWithAim(12, 5, 0)).move(new Move(Direction.Down, 3))).to.eql(new PositionWithAim(12, 5, 3));
+      expect((new PositionWithAim(12, 5, 0)).move(new Move(Direction.Forward, 2))).to.eql(new PositionWithAim(12, 7, 0));
+      expect((new PositionWithAim(12, 5, 3)).move(new Move(Direction.Forward, 2))).to.eql(new PositionWithAim(18, 7, 3));
+    })
+    it("calculates position", () => {
+      expect(calculatePositionWithAim(exampleMoves)).to.eql(new PositionWithAim(60, 15, 10));
+    })
+    const answer = 1963088820;
+    it(`is ${answer}`, () => {
+      expect(calculatePositionWithAim(realMoves)).to.eql(new PositionWithAim(991459, 1980, 866));
+      expect(991459 * 1980).to.eql(answer);
     })
   });
 });
